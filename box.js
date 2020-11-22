25f78cbfe125b65a97ec757cb897894e375d95ca
Jiyaparsana <73345190+Jiyaparsana@users.noreply.github.com>
2020-11-22 18:24:15 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,15 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height,angle){
+    super(x,y,width,height,angle);
+  }
+  display(){
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+   rectMode(CENTER)
+   rect(0,0,this.width,this.height)
+    pop();
+  }
+
+};
